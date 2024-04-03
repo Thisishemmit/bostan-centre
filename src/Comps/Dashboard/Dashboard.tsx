aaac1fe7fc28d7c459b07e3191610a96b70308f6
Thisishemmit <mkaroumi123@gmail.com>
2024-04-03 23:41:45 +0000

    Dashboard: boxTable need tooltips for Résumé cells

--- src/Comps/Dashboard/Dashboard.tsx
+++ src/Comps/Dashboard/Dashboard.tsx
@@ -92,11 +92,22 @@ const BoxD = () => (
     </Card>
 )
 
+const BoxE = () => (
+    <Card className="col-span-2">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-lg font-light">
+                Statistiques des non-payeurs
+            </CardTitle>
+            <DollarSign className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+    </Card>
+)
+
 const BoxTable = () => (
     <Card className="2xl:col-span-2">
         <CardHeader className="flex flex-row items-center">
             <div className="grid gap-2">
-                <CardTitle className="text-4xl">Liste des étudiants</CardTitle>
+                <CardTitle className="text-3xl">Liste des étudiants</CardTitle>
                 <CardDescription className="font-medium">
                     Étudiants récemment inscrits
                 </CardDescription>
@@ -285,96 +296,9 @@ const BoxTable = () => (
 const BoxNonPaye = () => (
     <Card>
         <CardHeader>
-            <CardTitle>Recent Sales</CardTitle>
+            <CardTitle className="text-3xl">Résumé du non-payeur</CardTitle>
         </CardHeader>
-        <CardContent className="grid gap-8">
-            <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/01.png" alt="Avatar" />
-                    <AvatarFallback>OM</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        Olivia Martin
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                        olivia.martin@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$1,999.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/02.png" alt="Avatar" />
-                    <AvatarFallback>JL</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        Jackson Lee
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                        jackson.lee@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$39.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/03.png" alt="Avatar" />
-                    <AvatarFallback>IN</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        Isabella Nguyen
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                        isabella.nguyen@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$299.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/04.png" alt="Avatar" />
-                    <AvatarFallback>WK</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        William Kim
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                        will@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$99.00</div>
-            </div>
-            <div className="flex items-center gap-4">
-                <Avatar className="hidden h-9 w-9 sm:flex">
-                    <AvatarImage src="/avatars/05.png" alt="Avatar" />
-                    <AvatarFallback>SD</AvatarFallback>
-                </Avatar>
-                <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        Sofia Davis
-                    </p>
-                    <p className="text-sm text-muted-foreground">
-                        sofia.davis@email.com
-                    </p>
-                </div>
-                <div className="ml-auto font-medium">+$39.00</div>
-            </div>
-        </CardContent>
-    </Card>
-)
-
-const BoxE = () => (
-    <Card className="col-span-2">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-lg font-light">
-                Statistiques des non-payeurs
-            </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
+        <CardContent className="grid gap-8"></CardContent>
     </Card>
 )
 
